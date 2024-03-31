d9ef17a1889de322e2ab2d2cdac4c4e24c73927b
faith-alimi <faith.alimi@juicymedia.co.uk>
2024-03-31 12:44:48 +0100

    client v2

---
+++ client/.angular/cache/17.3.1/vite/deps/ng-zorro-antd_icon.js
@@ -0,0 +1,29 @@
+import {
+  DEFAULT_TWOTONE_COLOR,
+  NZ_ICONS,
+  NZ_ICONS_PATCH,
+  NZ_ICONS_USED_BY_ZORRO,
+  NZ_ICON_DEFAULT_TWOTONE_COLOR,
+  NzIconDirective,
+  NzIconModule,
+  NzIconPatchService,
+  NzIconService
+} from "./chunk-QROX7CZ7.js";
+import "./chunk-BSJ72W4M.js";
+import "./chunk-2UYILFEK.js";
+import "./chunk-UJCAIR6G.js";
+import "./chunk-L5HZ4PYD.js";
+import "./chunk-VJFU7J2G.js";
+import "./chunk-J4B6MK7R.js";
+export {
+  DEFAULT_TWOTONE_COLOR,
+  NZ_ICONS,
+  NZ_ICONS_PATCH,
+  NZ_ICONS_USED_BY_ZORRO,
+  NZ_ICON_DEFAULT_TWOTONE_COLOR,
+  NzIconDirective,
+  NzIconModule,
+  NzIconPatchService,
+  NzIconService
+};
+//# sourceMappingURL=ng-zorro-antd_icon.js.map
