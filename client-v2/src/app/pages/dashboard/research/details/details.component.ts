4d19dcfad59f1165fa9344ea939a5400a6d8f0d1
Alimi Faith <73644238+ftoucch@users.noreply.github.com>
2025-01-30 20:25:38 +0000

    Ui cleanup

--- client-v2/src/app/pages/dashboard/research/details/details.component.ts
+++ client-v2/src/app/pages/dashboard/research/details/details.component.ts
@@ -1,16 +1,18 @@
 import { Component, ViewChild } from '@angular/core';
 import { ResearchService } from '../../../../services/research.service';
 import { ActivatedRoute } from '@angular/router';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { CreateQueryComponent } from '../../../../modals/create-query/create-query.component';
-import { NzSwitchModule } from 'ng-zorro-antd/switch';
-import { CommonModule } from '@angular/common';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { CommonModule } from '@angular/common';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { FormsModule } from '@angular/forms';
+import { CreateQueryComponent } from '../../../../modals/create-query/create-query.component';
 import { EmptyComponent } from '../../../../components/empty/empty.component';
 import { ChatboxComponent } from '../../../../components/chatbox/chatbox.component';
-import { FormsModule } from '@angular/forms';
+import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
+
 @Component({
   selector: 'app-details',
   standalone: true,
@@ -18,26 +20,28 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     NzIconModule,
     NzInputModule,
+    NzSwitchModule,
+    NzSkeletonModule,
+    FormsModule,
     CreateQueryComponent,
     EmptyComponent,
-    ChatboxComponent,
-    NzSwitchModule,
-    FormsModule
+    ChatboxComponent
   ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css',
 })
 export class DetailsComponent {
-  @ViewChild(CreateQueryComponent, { static: false })
-  createQueryModal!: CreateQueryComponent;
-  @ViewChild(ChatboxComponent, { static: false })
-  openChatBox!: ChatboxComponent;
-  emptyModal!: EmptyComponent;
+  @ViewChild(CreateQueryComponent, { static: false }) createQueryModal!: CreateQueryComponent;
+  @ViewChild(ChatboxComponent, { static: false }) openChatBox!: ChatboxComponent;
+
   research: any;
-  researchId: string = ' ';
+  researchId: string = '';
   filterQueries: Array<any> = [];
   primaryStudies: Array<any> = [];
   showUnfilteredPapers = false;
+  loading = false; // Track loading state
+  expandedAbstracts: { [key: number]: boolean } = {}; // Track expanded abstracts
+
   constructor(
     private route: ActivatedRoute,
     private researchService: ResearchService,
@@ -49,43 +53,39 @@ export class DetailsComponent {
     this.getAllQuery();
     this.getAllPrimaryStudies();
   }
+
   getResearch() {
+    this.loading = true;
     this.researchService.getResearch(this.researchId).subscribe({
       next: (res: any) => {
         this.research = res;
-        if(res.length == 0) {
-          this.notification.create(
-            'Sorry',
-            'Sorry',
-            'there are no reseearch matching your criteria'
-          );
+        this.loading = false;
+        if (res.length === 0) {
+          this.notification.create('info', 'No Research Found', 'No research matches your criteria.');
         }
       },
+      error: () => (this.loading = false),
     });
   }
+
   getAllQuery() {
     this.researchService.getAllQuery(this.researchId).subscribe({
       next: (res: any) => {
-        if(res.data)
-        this.filterQueries = res.data;
+        this.filterQueries = res.data || [];
       },
     });
   }
+
   deleteQuery(queryID: any) {
     this.modal.confirm({
-      nzTitle: 'Are you sure you want to delete this Query ?',
-      nzContent:
-        'Dleteing this query will also remove all associated research papers',
+      nzTitle: 'Are you sure you want to delete this Query?',
+      nzContent: 'Deleting this query will also remove all associated research papers.',
       nzOkText: 'Delete',
       nzOkDanger: true,
       nzOnOk: () => {
         this.researchService.deleteQuery(queryID).subscribe({
-          next: (res: any) => {
-            this.notification.create(
-              'success',
-              'Success',
-              'Query was successfully deleted'
-            );
+          next: () => {
+            this.notification.create('success', 'Success', 'Query successfully deleted.');
             this.getAllQuery();
             this.getAllPrimaryStudies();
           },
@@ -93,35 +93,51 @@ export class DetailsComponent {
       },
     });
   }
+
   getAllPrimaryStudies() {
+    this.loading = true;
     this.researchService.getAllPrimaryStudies(this.researchId).subscribe({
       next: (res: any) => {
-        this.primaryStudies = res.data;
+        this.primaryStudies = res.data || [];
+        this.loading = false;
       },
+      error: () => (this.loading = false),
     });
   }
 
   getUnfilteredPaper() {
+    this.loading = true;
     this.researchService.getUnfilteredPapers(this.researchId).subscribe({
       next: (res: any) => {
-        this.primaryStudies = res.data
-      }
-    })
+        this.primaryStudies = res.data || [];
+        this.loading = false;
+      },
+      error: () => (this.loading = false),
+    });
   }
+
   showCreateQueryModal() {
     this.createQueryModal.showModal();
   }
+
   showChatBox() {
     this.openChatBox.open();
   }
 
   onSwitchChange(switchState: boolean): void {
-    if (switchState) {
-      this.getUnfilteredPaper();
-    }
-    else {
-      this.getAllPrimaryStudies();
-    }
+    switchState ? this.getUnfilteredPaper() : this.getAllPrimaryStudies();
   }
 
+  // Toggle abstract expansion
+  toggleAbstract(index: number) {
+    this.expandedAbstracts[index] = !this.expandedAbstracts[index];
+  }
+
+  // Function to truncate abstract to 20 words
+  truncateText(text: string, index: number): string {
+    if (!text) return 'Abstract not available';
+    if (this.expandedAbstracts[index]) return text; // Show full text if expanded
+    const words = text.split(' ');
+    return words.length > 20 ? words.slice(0, 20).join(' ') + '...' : text;
+  }
 }
