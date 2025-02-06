5e29d4205b78dc27b4ce72c9707e1272ae9d68bc
Alimi Faith <73644238+ftoucch@users.noreply.github.com>
2025-02-06 16:25:34 +0000

    optimize query components

---
+++ client-v2/src/app/components/forms/create-query/create-query.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-query',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-query.component.html',
+  styleUrl: './create-query.component.css'
+})
+export class CreateQueryComponent {
+
+}
