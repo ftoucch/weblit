1701fb1a77f0f57396f2c97fd2c0127287429407
Fatai Alimi <73644238+ftoucch@users.noreply.github.com>
2026-03-04 22:05:49 +0000

    added dependency to DB

---
+++ app/db/config.py
@@ -0,0 +1,41 @@
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase, AsyncIOMotorCollection
+from app.core.config import config
+import logging
+
+logger = logging.getLogger(__name__)
+
+class MongoDB:
+    client: AsyncIOMotorClient | None = None
+    db: AsyncIOMotorDatabase | None = None
+    collections: dict[str, AsyncIOMotorCollection] | None = None
+
+mongo_db = MongoDB()
+
+async def connect_mongo() -> None:
+    logger.info("Connecting to MongoDB...")
+    mongo_db.client = AsyncIOMotorClient(
+        config.db_url,
+        serverSelectionTimeoutMS=5000
+    )
+    
+    mongo_db.db = mongo_db.client[config.mongo_db]
+    await mongo_db.client.admin.command('ping')
+    logger.info("Successfully connected to MongoDB.")
+
+    mongo_db.collections = {
+        "users": mongo_db.db["users"]
+    }
+
+    await mongo_db.db["users"].create_index("email", unique=True)
+
+    logger.info("MongoDB collections and indexes are set up.")
+
+async def close_mongo() -> None:
+    if mongo_db.client:
+        mongo_db.client.close()
+        logger.info("MongoDB connection closed.")
+
+def get_db() -> AsyncIOMotorDatabase:
+    if mongo_db.db is None:
+        raise Exception("MongoDB is not connected.")
+    return mongo_db.db
