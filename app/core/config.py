5eaf874567001a2b0e8a8516b15c5cea499f06b0
Fatai Alimi <73644238+ftoucch@users.noreply.github.com>
2026-03-04 21:45:14 +0000

    added config files

---
+++ app/core/config.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+import secrets
+
+load_dotenv()
+
+
+class Config(BaseSettings):
+    app_name: str = "weblit"
+    debug: bool = False
+    mongo_user: str = ""
+    mongo_password: str = ""
+    mongo_db: str = ""
+    mongo_host: str = "mongo"
+    mongo_port: int = 27017
+    secret_key: str = secrets.token_urlsafe(32)
+    access_token_expire_minutes: int = 60 * 24 * 7  # 7 days
+
+    @property
+    def db_url(self):
+        return (
+            f"mongodb://{self.mongo_user}:{self.mongo_password}"
+            f"@{self.mongo_host}:{self.mongo_port}/{self.mongo_db}"
+            "?authSource=admin"
+        )
+    
+config = Config()
