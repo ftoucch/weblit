8c24ed94548a285d93390aaa0ffaa75026153987
Fatai Alimi <73644238+ftoucch@users.noreply.github.com>
2026-03-11 16:09:58 +0000

    started adding frontend

---
+++ frontend/src/lib/components/FormInputs/LoadingButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+ export let label: string
+ export let loading: boolean = false
+</script>
+<button
+  type="submit"
+  class="flex w-full items-center gap-2 justify-center cursor-pointer rounded-sm bg-weblit px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-weblit-dark focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 h-10"
+  disabled={loading}
+>
+  {#if loading}
+    <div class="btn-spinner"></div>
+    {:else}
+      {label}
+  {/if}
+</button>
---
+++ frontend/src/lib/components/FormInputs/TextInput.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  export let label: string
+  export let name: string
+  export let type: string
+  export let value: string = ''
+  export let placeholder: string = ''
+  export let error: string| undefined = undefined
+</script>
+
+<div>
+  <div class="flex items-center justify-between">
+    <label for={name} class="block text-sm/6 font-medium text-gray-900">
+      {label}
+    </label>
+  </div>
+
+  <div class="mt-2">
+    <input
+      type={type}
+      name={name}
+      id={name}
+      bind:value
+      placeholder={placeholder}
+      autocomplete="current-password"
+      required
+      class="block w-full rounded-sm bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-freductive-600 sm:text-sm/6 h-10"
+    />
+  </div>
+
+  {#if error}
+    <div class="mt-5 form-error text-xs text-red-500">{error}</div>
+  {/if}
+</div>
