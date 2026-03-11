67b995f2ee46bc3300d4e4d89ac6bdb37b00dcda
Fatai Alimi <73644238+ftoucch@users.noreply.github.com>
2026-03-11 16:09:58 +0000

    started adding frontend

---
+++ frontend/src/routes/auth/login/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import TextInput from "$lib/components/FormInputs/TextInput.svelte";
+    import LoadingButton from "$lib/components/FormInputs/LoadingButton.svelte";
+    let loading = false
+</script>
+<form class="space-y-6" method="post">
+      <TextInput  label="Email" name="email" type="email" />
+      <TextInput  label="Password" name="password" type="email" error="" />
+      <div>
+        <LoadingButton label="Sign in" {loading} />
+      </div>
+</form>
+<div class="text-sm mt-5">
+    <a href="/forgot-password" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+</div>
+<p class="mt-5 text-sm/6 text-gray-500">
+    Not a member?
+<a href="register" class="font-semibold text-indigo-600 hover:text-indigo-500">Register</a>
+</p>
---
+++ frontend/src/routes/auth/register/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import TextInput from "$lib/components/FormInputs/TextInput.svelte";
+    import LoadingButton from "$lib/components/FormInputs/LoadingButton.svelte";
+    let loading = false
+</script>
+<form class="space-y-6" method="post">
+    <TextInput label="Name" name="name" type="text" error="" />
+    <TextInput  label="Email" name="email" type="email" />
+    <TextInput  label="Password" name="password" type="email" error="" />
+    <div>
+        <LoadingButton label="Register" {loading} />
+    </div>
+</form>
+<div class="text-sm mt-5">
+    <a href="/forgot-password" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+</div>
+<p class="mt-5 text-sm/6 text-gray-500">
+    Already a member?
+<a href="login" class="font-semibold text-indigo-600 hover:text-indigo-500">Login</a>
+</p>
