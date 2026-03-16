9684108c500b9685ebf77229c4215b203da822d8
Fatai Alimi <73644238+ftoucch@users.noreply.github.com>
2026-03-16 16:04:18 +0000

    added search page

---
+++ frontend/src/routes/search/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang='ts'>
+    import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+    import Header from "$lib/components/Header.svelte";
+
+    let { children } = $props();
+</script>
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<main>
+    <Header />
+    <section>
+        {@render children()}
+    </section>
+</main>
