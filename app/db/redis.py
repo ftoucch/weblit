18516a29de8c4227b6488e55ba145389e826ee66
Fatai Alimi <73644238+ftoucch@users.noreply.github.com>
2026-04-09 16:43:46 +0100

    fix redis url

--- app/db/redis.py
+++ app/db/redis.py
@@ -1,23 +1,37 @@
 import logging
-
 import redis.asyncio as redis
-from app.core.config import config 
+from app.core.config import config
 
 logger = logging.getLogger(__name__)
 
-redis_client: redis.Redis = redis.Redis(
-    host = config.redis_host,
-    port = config.redis_port,
-    db=2,
-    decode_responses=True
-)
+redis_client: redis.Redis | None = None
 
 async def connect_redis() -> None:
+    global redis_client
     logger.info("Connecting to Redis...")
+
+    if config.redis_url:
+        redis_client = redis.from_url(
+            config.redis_url,
+            decode_responses=True,
+        )
+    else:
+        redis_client = redis.Redis(
+            host=config.redis_host,
+            port=config.redis_port,
+            db=2,
+            decode_responses=True,
+        )
+
     await redis_client.ping()
     logger.info("Successfully connected to Redis.")
 
-
 async def close_redis() -> None:
-    await redis_client.aclose()
-    logger.info("Redis connection closed.")
+    if redis_client:
+        await redis_client.aclose()
+        logger.info("Redis connection closed.")
+
+def get_redis() -> redis.Redis:
+    if redis_client is None:
+        raise RuntimeError("Redis not initialised.")
+    return redis_client
